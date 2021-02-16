486bf26e62a164b388981c0857dd4fe9ab949fae
renegng <renegng@gmail.com>
2021-02-16 02:11:36 -0600

    Stable alpha of appointments

--- static/js/appointments-emp.js
+++ static/js/appointments-emp.js
@@ -1,5 +1,6 @@
 /************************** EMPLOYEE APPOINTMENTS **************************/
 import * as cscApiKey from '../../instance/js/swing_countrycitystate-api-key.json';
+import { MDCMenu, Corner } from '@material/menu';
 
 
 /************************** FUNCTIONS **************************/
@@ -44,11 +45,12 @@ window.getStates = getStates;
 // Load Cities
 export function loadCities(data) {
     let citListEl = document.querySelector('#f-appointment-city-select');
+    let sortedData = data.sort((a, b) => a.name < b.name ? -1 : 1);
 
     mdcAssignedVars['mdcCitiesSelect'].selectedIndex = -1;
     citListEl.innerHTML = '';
 
-    data.forEach((cit) => {
+    sortedData.forEach((cit) => {
         let citContainer = document.createElement('li');
         let citName = document.createElement('span');
 
@@ -56,7 +58,7 @@ export function loadCities(data) {
         citName.classList.add('mdc-list-item__text');
 
         citName.textContent = cit.name;
-        citContainer.setAttribute('data-value', cit.iso2);
+        citContainer.setAttribute('data-value', cit.id);
 
         citContainer.appendChild(citName);
         citListEl.appendChild(citContainer);
@@ -69,8 +71,9 @@ window.loadCities = loadCities;
 // Load Departments
 export function loadDepartments(data) {
     let depListEl = document.querySelector('#f-appointment-department-select');
+    let sortedData = data.sort((a, b) => a.name < b.name ? -1 : 1);
 
-    data.forEach((dep) => {
+    sortedData.forEach((dep) => {
         let depContainer = document.createElement('li');
         let depName = document.createElement('span');
 
@@ -88,6 +91,32 @@ export function loadDepartments(data) {
 /* Allow 'window' context to reference the function */
 window.loadDepartments = loadDepartments;
 
+// Load User Information
+export function loadUserInfo(evt, uType) {
+    let usrName = evt.detail.item.querySelector('.mdc-list-item__primary-text').textContent;
+    let usrEmail = evt.detail.item.querySelector('.mdc-list-item__secondary-text').textContent;
+
+    if (uType == 'usr') {
+        let usrCont = document.querySelector('.container-appointmentadm-user--record-info');
+
+        usrCont.querySelector('.mdc-typography--caption').textContent = usrName;
+        usrCont.querySelector('.mdc-typography--subtitle2').textContent = usrEmail;
+        usrCont.classList.remove('container--hidden');
+
+        document.querySelector('.container-appointment-confirm--user').textContent = usrName;
+    } else {
+        let empCont = document.querySelector('.container-appointmentadm-emp--record-info');
+
+        empCont.querySelector('.mdc-typography--caption').textContent = usrName;
+        empCont.querySelector('.mdc-typography--subtitle2').textContent = usrEmail;
+        empCont.classList.remove('container--hidden');
+
+        document.querySelector('.container-appointment-confirm--emp').textContent = usrName;
+    }
+}
+/* Allow 'window' context to reference the function */
+window.loadUserInfo = loadUserInfo;
+
 // Select Appointment Service
 export function selectAppointmentService(value) {
     let serviceConfirmEl = document.querySelector('.container-appointment-confirm--service');
@@ -98,12 +127,60 @@ export function selectAppointmentService(value) {
 window.selectAppointmentService = selectAppointmentService;
 
 // Select Appointmemt Time
-export function selectAppointmentTime(time) {
+export function selectAppointmentTime(elm) {
+    let curSelected = document.querySelector('.mdc-card--selected');
     let timeConfirmEl = document.querySelector('.container-appointment-confirm--time');
-    timeConfirmEl.textContent = time;
+    let timeSelectedEl = elm.querySelector('.container-appointment-hours--time').textContent;
+
+    if (curSelected) {
+        curSelected.classList.remove('mdc-card--selected');
+        curSelected.querySelector('.container-appointment-hours--time').classList.remove('s-font-color-primary');
+        curSelected.querySelector('.container-appointment-hours--time').classList.add('s-font-color-secondary');
+    }
+    
+    elm.classList.add('mdc-card--selected');
+    elm.querySelector('.container-appointment-hours--time').classList.remove('s-font-color-secondary');
+    elm.querySelector('.container-appointment-hours--time').classList.add('s-font-color-primary');
+
+    timeConfirmEl.textContent = timeSelectedEl;
 }
 /* Allow 'window' context to reference the function */
 window.selectAppointmentTime = selectAppointmentTime;
+
+// Set Current Selected Date on Hours Cards
+export function setSelectedDateOnUI(date) {
+    let formatDate = swcms.returnFormatDate(date, 'date');
+    let curSelected = document.querySelector('.mdc-card--selected');
+
+    if (curSelected) {
+        let timeConfirmEl = document.querySelector('.container-appointment-confirm--time');
+
+        curSelected.classList.remove('mdc-card--selected');
+        curSelected.querySelector('.container-appointment-hours--time').classList.remove('s-font-color-primary');
+        curSelected.querySelector('.container-appointment-hours--time').classList.add('s-font-color-secondary');
+
+        timeConfirmEl.textContent = '';
+    }
+
+    document.querySelector('.container-appointment-confirm--date').textContent = formatDate;
+    document.querySelector('#appointment-hours').querySelectorAll('.container-appointment-hours--date').forEach((el) => {
+        el.textContent = formatDate;
+    });
+}
+
+
+/************************** MATERIAL DESIGN COMPONENTS INIT **************************/
+
+// Material Menu
+var searchResultsUsers = new MDCMenu(document.querySelector('#searchUsersMenu'));
+searchResultsUsers.setAnchorCorner(Corner.BOTTOM_START);
+document.getElementById('f-appointment-ufilter-input').addEventListener('click', () => (searchResultsUsers.open = !searchResultsUsers.open));
+document.querySelector('#searchUsersMenu').addEventListener('MDCMenu:selected', (evt) => { loadUserInfo(evt, 'usr') });
+
+var searchResultsEmployees = new MDCMenu(document.querySelector('#searchEmployeesMenu'));
+searchResultsEmployees.setAnchorCorner(Corner.BOTTOM_START);
+document.getElementById('f-appointment-efilter-input').addEventListener('click', () => (searchResultsEmployees.open = !searchResultsEmployees.open));
+document.querySelector('#searchEmployeesMenu').addEventListener('MDCMenu:selected', (evt) => { loadUserInfo(evt, 'emp') });
 
 
 /************************** ON PAGE LOAD **************************/
@@ -126,9 +203,8 @@ window.addEventListener('load', () => {
     
     // Click on date behavior
     appCal.onDateClick((event, date) => {
-        let dateConfirmEl = document.querySelector('.container-appointment-confirm--date');
         appCal.set(date);
-        dateConfirmEl.textContent = swcms.returnFormatDate(date, 'date');
+        setSelectedDateOnUI(date);
     });
 
     // Make changes on the date elements
@@ -149,6 +225,10 @@ window.addEventListener('load', () => {
             } else {
                 element.classList.add('jsCalendar-date-available');
             }
+        }
+        // Display the current date on the hours cards
+        if (info.isCurrent) {
+            setSelectedDateOnUI(date);
         }
     });
 
--- views/home.py
+++ views/home.py
@@ -4,7 +4,7 @@ from datetime import timezone as tz
 from flask import Blueprint, redirect, render_template, request, url_for, jsonify, make_response
 from flask import current_app as app
 from flask_login import logout_user, current_user, login_required
-from models.models import User, CatalogUserRoles, UserXRole
+from models.models import CatalogServices, CatalogUserRoles, User, UserXRole
 
 home = Blueprint('home', __name__, template_folder='templates', static_folder='static')
 
@@ -38,7 +38,8 @@ def _appointmentscreate():
 @login_required
 def _appointmentscreateadmin():
     app.logger.debug('** SWING_CMS ** - Crear Citas Admin')
-    return render_template('appointments_create_admin.html')
+    srv = CatalogServices.query.filter_by(enabled = True).order_by(CatalogServices.name.asc())
+    return render_template('appointments_create_admin.html', services=srv)
 
 
 @home.route('/chat/')
